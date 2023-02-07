6a9d1640e205c72478440e461cf49e3a97b5a94a
AllaSn <you@example.com>
2023-02-07 09:35:06 +0300

    1

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = 2;
+const screens = 3;
+const screenPrice = 4;
+const rollback = 5;
+const fullPric = 6;
+const adaptive = 7;
+
+
+let bebebebe = 30;
+console.log(bebebebe);
+alert ("bzzzzzzzzzz");
